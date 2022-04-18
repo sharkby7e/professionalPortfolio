4feb1d5a36456e66bebdf8095902c01d39cded67
sharkbyte <sgquin@gmail.com>
2022-04-18 16:40:42 -0700

    finish portfolio page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
 console.log("script is linked")
+
+$(document).ready(function(){
+  $('.carousel').carousel({
+    fullWidth: true,
+    indicators: true
+  });
+});
+
+// $('.carousel.carousel-slider').carousel({
+//   fullWidth: true
+// });
